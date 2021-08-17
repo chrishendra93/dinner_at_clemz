952d32ab3211df17d777810d94eb1d55d71d0460
chrishendra93 <chrishendra93@gmail.com>
2021-08-18 01:17:51 +0800

    update README and add additional functionalities to main.py

--- README.md
+++ README.md
@@ -14,10 +14,21 @@ dinner_at_clemz requires [Python3](https://www.python.org). To install the lates
 $ pip install dinner_at_clemz
 ```
 
-After installation, run the following command:
+After installation, run the following command to display all messages
 
 ```sh
 $ display_message
+```
+
+Alternatively, one can supply the --name flag to display only selected messages, for example:
+
+```sh
+$ display_message --name cuki cuku cukcuk
+```
+To see all possible values for --name run:
+
+```sh
+$ display_message --help
 ```
 ### License
 MIT
--- src/main.py
+++ src/main.py
@@ -9,22 +9,24 @@ def argparser():
         formatter_class=ArgumentDefaultsHelpFormatter,
         add_help=False
     )
-    parser.add_argument("--name", default='all', required=False, 
-                        help="Display the message from the given name",
-                        choices=[x.lower() for x in messages.__all__] + ['all'])
+    parser.add_argument("--name", "-n", default=None, required=False, 
+                        help="Display the message from the given names",
+                        nargs='+',
+                        choices=[x.lower() for x in messages.__all__])
     parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS)
     return parser
 
 
 def main():
     args = argparser().parse_args()
-    name = args.name
+    names = args.name
     all_messages = {name.lower(): messages.__dict__.get(name) for name in messages.__all__}
-    if name == 'all':
+    if names is None:
         for _, tomodachi in all_messages.items():
             tomodachi().show_message()
     else:
-        all_messages[name]().show_message()
+        for name in names:
+            all_messages[name]().show_message()
 
 
 if __name__ == '__main__':
