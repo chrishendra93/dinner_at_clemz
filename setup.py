a884973f257bb18584a5b69d627d01f82573a082
chrishendra93 <chrishendra93@gmail.com>
2021-08-18 01:32:08 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open('README.md') as f:
 
 setup(
     author="meja_bundar_dover_crescent",
-    maintainer_email="dinner_at_clementi@clementi_mail.com",
+    maintainer_email="dinner_at_clementi@gmail.com",
     name=__pkg_name__,
     license="MIT",
     description='dinner_at_clementi is a python package that is essential for launching the next scalable unicorn in South East Asia.',
     version='v0.0.1',
     long_description=README,
     long_description_content_type='text/markdown',
-    url='https://github.com/chrishendra93/xpore',
+    url='https://github.com/chrishendra93/dinner_at_clemz',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
